1d2869bf8fe135848e97db440f53665263c04231
李俊 <lijun@021.com>
2019-09-10 11:25:25 +0800

    调试

--- src/js/main.js
+++ src/js/main.js
@@ -545,9 +545,10 @@ $(function () {
             console.log("天啦，偶买噶，你竟然还在用IE？")
         } else {
             ! function (e, t, a) {
-                console.log(e,t,a)
                 function r() {
-                    for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body && t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y-- ,
+                    console.log(e,t,a)
+
+                    for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y-- ,
                         s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e]
                             .y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale +
                         ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
