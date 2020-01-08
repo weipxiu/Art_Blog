89cebc39660398f69819e6100c598687f39d3ad2
李俊 <lijun@021.com>
2020-01-08 10:55:11 +0800

    优化侧边栏评论头像

--- src/sidebar.php
+++ src/sidebar.php
@@ -172,7 +172,7 @@
             $output = $pre_HTML;
             foreach ($comments as $comment) { $com_excerpt = strip_tags($comment->com_excerpt); $excerpt_len = mb_strlen($com_excerpt, 'utf-8');
                 if ($excerpt_len > 46) $com_excerpt = mb_substr($com_excerpt, 0, 46, 'utf-8').'...';
-                $output .= "\n<li>". '<img src=https://q.qlogo.cn/headimg_dl?bs=qq&dst_uin='.get_comment_author_email().'&src_uin=qq.feixue.me&fid=blog&spec=100&id='.rand(1,1000).'>' .strip_tags($comment->comment_author) . "<span>（" . timeago($comment->comment_date_gmt) . "）</span>" . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
+                $output .= "\n<li>". '<img src=https://q.qlogo.cn/headimg_dl?bs=qq&dst_uin='.get_comment_author_email().'&src_uin=qq.feixue.me&fid=blog&spec=100&id='.rand(1,1000).' onerror="this.src=\'https://www.weipxiu.com/wp-content/themes/Art_Blog/images/head_portrait.jpg\';">' .strip_tags($comment->comment_author) . "<span>（" . timeago($comment->comment_date_gmt) . "）</span>" . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
             $output .= $post_HTML;
             $output = convert_smilies($output);
             echo $output;
