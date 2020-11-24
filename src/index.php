0e0258d565bee69512c8bbdd1377a5829efc3a2e
李俊 <lijun@021.com>
2020-11-24 09:56:01 +0800

    优化特色图片

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='Art_Blog v2020-11-23'>
+<meta name='generator' content='Art_Blog v2020-11-24'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
