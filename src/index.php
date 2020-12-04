cb2493e489d715d7e85c66eff99fd4654a559e8e
李俊 <lijun@021.com>
2020-12-04 10:29:28 +0800

    优化无序列表

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2020-12-03 19:08:01'>
+<meta name='generator' content='WordPress/Art_Blog v2020-12-04 10:29:15'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
