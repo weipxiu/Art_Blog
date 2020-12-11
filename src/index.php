38bd390eaa8727a2b15a9bc4760e970a9c8e64e2
李俊 <lijun@021.com>
2020-12-11 18:39:58 +0800

    修改底部

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2020-12-11 17:17:13'>
+<meta name='generator' content='WordPress/Art_Blog v2020-12-11 18:39:10'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
