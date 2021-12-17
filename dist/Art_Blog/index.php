2e3c9be8592dca3d472ee04131fe522ea8d4068f
李俊 <lijun@021.com>
2021-12-17 19:11:11 +0800

    fix: 修复关于博客页面样式

--- dist/Art_Blog/index.php
+++ dist/Art_Blog/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2021-12-17 18:39:04'>
+<meta name='generator' content='WordPress/Art_Blog v2021-12-17 19:10:16'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
