1f247e56472f013d7d51248cf0223f63a516397f
李俊 <lijun@021.com>
2021-12-20 18:57:54 +0800

    fix: 修复样式bug

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2021-12-20 18:25:46'>
+<meta name='generator' content='WordPress/Art_Blog v2021-12-20 18:57:38'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
