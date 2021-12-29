c013d29e7bbf7a77d220a0054bde06420029ba90
李俊 <lijun@021.com>
2021-12-29 18:42:50 +0800

    feat: 减少跨域请求

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2021-12-29 16:24:51'>
+<meta name='generator' content='WordPress/Art_Blog v2021-12-29 18:40:34'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
