cca5fc7ca2a05cd5f15e5775ab9bb8be80954391
李俊MacBook <lijun@021.com>
2022-05-14 23:26:57 +0800

    style: 优化样式

--- dist/Art_Blog/index.php
+++ dist/Art_Blog/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2022-05-14 20:36:29'>
+<meta name='generator' content='WordPress/Art_Blog v2022-05-14 23:25:37'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
