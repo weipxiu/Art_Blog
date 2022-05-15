18d32f7f671f726a1e333e8bb0fffb73992aaffd
李俊MacBook <lijun@021.com>
2022-05-15 12:01:49 +0800

    style: 提升代码健壮性

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2022-05-15 09:18:51'>
+<meta name='generator' content='WordPress/Art_Blog v2022-05-15 12:00:07'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
