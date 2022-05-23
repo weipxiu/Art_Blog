13db2c4edb4c6011591b401e13bb6c647266001a
李俊 <lijun@021.com>
2022-05-23 14:09:25 +0800

    feat: 优化icon

--- dist/Art_Blog/index.php
+++ dist/Art_Blog/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2022-05-23 09:57:54'>
+<meta name='generator' content='WordPress/Art_Blog v2022-05-23 14:07:30'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
