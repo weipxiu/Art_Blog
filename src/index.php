b25269fe1a983821e486f4d1b351cb621ad322d4
李俊 <lijun@021.com>
2022-05-23 18:47:18 +0800

    feat: 优化icon

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2022-05-23 17:32:07'>
+<meta name='generator' content='WordPress/Art_Blog v2022-05-23 17:59:07'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
