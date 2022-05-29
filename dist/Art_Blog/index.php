616b0ff4c1408580b225a427ff3b50f2a0718852
李俊 <lijun@021.com>
2022-05-29 23:19:27 +0800

    feat: 头部固定采用sticky

--- dist/Art_Blog/index.php
+++ dist/Art_Blog/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2022-05-29 20:35:05'>
+<meta name='generator' content='WordPress/Art_Blog v2022-05-29 22:25:13'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
