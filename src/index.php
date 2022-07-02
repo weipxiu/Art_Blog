7bc420dac4d77c3aad85e320b8ed104d9b2c1da8
李俊 <lijun@021.com>
2022-07-02 09:18:14 +0800

    feat: 重新打包

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name='generator' content='WordPress/Art_Blog v2022-07-02 08:59:50'>
+<meta name='generator' content='WordPress/Art_Blog v2022-07-02 09:17:59'>
 <title><?php $name = wp_title( '-', true, 'right' );
 		if ($name) {
 			echo $name . "&nbsp;-&nbsp;" . get_bloginfo('description');
